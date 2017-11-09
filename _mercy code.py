29b94fb92e2e2be5bc5b515965e9f8ee1a3857d4
Mercyf04 <33519488+Mercyf04@users.noreply.github.com>
2017-11-09 09:05:07 -0500

    Initial Commit

---
+++ _mercy code.py
@@ -0,0 +1,41 @@
+print("What's your name?")
+name = input()
+
+print("Hello, " + name)
+
+
+
+print("What's your favorite color")
+color = input()
+
+if color == "yellow" or color == "white":
+    print("I love " + color + " too!")
+elif color == "blue":
+    print ("I also love blue!")
+else:
+    print("I think" + color + "is really pretty!")
+print("Do you go to school")
+school = input()
+
+if school == "yes":
+    print(school + ", Me too!")
+
+print ("What grade are you in?")
+grade = input()
+
+if grade == ("8th")
+    print ("I am also in 8th grade!")
+elif grade == "6th" or grade == "7th" or grade == "9th" or grade == "10th":
+    print ("cool!")
+print ("What's your favorite animal")
+
+favoriteanimal = input()
+else
+if favoriteanimal == "dogs"
+    print ("I love dogs! Do you have any pets?")
+    
+if favoriteanimal = 
+    print ("Cool! I love animals!")
+
+
+
---
+++ mercy mad lib.py
@@ -0,0 +1,31 @@
+
+
+print("Write a noun")
+noun1 = input()
+
+print("Write another noun(place)")
+noun2 = input()
+
+print("Write an adjective")
+adjective1 = input()
+
+print("Write a verb")
+verb1 = input()
+
+print("Write an adjective")
+adjective2 = input()
+
+print("Write a noun")
+noun3 = input ()
+
+print ("Write a place")
+place1 = input()
+
+print ("Write a plural noun")
+pluralnoun1 = input()
+### MAD LIB ###
+
+print("The " + adjective1 + " " + noun1 + " went to the " + noun2 + " to " + verb1 + ".")
+print("The " + adjective2 + " " + noun3 + " ate at the " + place1 + ".")
+print("After, the" + pluralnoun1 + "ran to the" + 
+#### BEGIN MADLIB ####

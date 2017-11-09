39ce2901653ca56c665d8d297d89c19d1e0bb425
Mercyf04 <33519488+Mercyf04@users.noreply.github.com>
2017-11-09 09:27:00 -0500

    Added more lines
    Made mad lib longer

--- mercy mad lib.py
+++ mercy mad lib.py
@@ -23,9 +23,32 @@ place1 = input()
 
 print ("Write a plural noun")
 pluralnoun1 = input()
+
+print ("Write a place")
+place2 = input()
+
+print ("Write the same place as before")
+place3 = input()
+
+print ("Write an animal")
+noun4 = input ()
+
+print ("Write a noun")
+noun5 = input ()
+
+print ("Write a adverb")
+adverb1 = input ()
+
+print ("Write a place")
+noun6 = input ()
+
+print ("Write and adverb")
+adverb2 = input ()
 ### MAD LIB ###
 
 print("The " + adjective1 + " " + noun1 + " went to the " + noun2 + " to " + verb1 + ".")
 print("The " + adjective2 + " " + noun3 + " ate at the " + place1 + ".")
-print("After, the" + pluralnoun1 + "ran to the" + 
+print("After, the " + pluralnoun1 + " ran to the" + place2 + "!")
+print("At the " + place3 + " there turned out to be a " + noun3 +"!")
+print("The " + noun5 + " was" + adverb1 + " to his" + noun6 + " and finally got to" + adverb2 + "."
 #### BEGIN MADLIB ####
